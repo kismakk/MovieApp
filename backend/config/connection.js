f1a6218bff328dffc4499b820232f1863b3c7954
Deforested <Teemuyritys@gmail.com>
2023-11-10 20:21:07 +0200

    Update connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+require('dotenv').config()
+const { Pool } = require('pg')
 const pgPool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PW,
   port: process.env.PG_PORT,
-  ssl: true,
-});
+  ssl: true
+})
 
 pgPool.connect((err) => {
   if (err) {
-    console.log(err.message);
+    console.log(err.message)
   } else {
-    console.log("Yhteys onnistui");
+    console.log('Yhteys onnistui')
   }
-});
+})
 
-module.exports = pgPool;
+module.exports = pgPool
