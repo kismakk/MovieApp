065f01ae39b435c6ae60f2b09d8450db4d7fa09d
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-13 19:19:42 +0200

    Modified createUser
    Added function to check if email is in use already and refactored code so it's easier to add more functions and that they can be exported with only 'user'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,22 @@
-const {createUser} = require('../models/userModel.js')
+const user = require('../models/userModel.js')
 
-const createUserController = async (req, res) => {
+const createUser = async (req, res) => {
     const userData = req.body;
     if (!userData.uname || !userData.pw || !userData.email) {
         return res.status(400).json({error: 'Missing required fields'});
     }
     try {
-        const result = await createUser(userData);
-        res.status(201).json(result);
+        const result = await user.createUser(userData);
+        res.status(201).json({message: 'User created succesfully'});
     } catch (error) {
-        console.log('Error in createUserController', error);
+        if (error.message === 'Email already in use') {
+            return res.status(400).json({error: error.message});
+        }
+        console.log('Error in createUser', error);
         res.status(500).json({error: 'Internal server error'});
     }
 }
 
 module.exports = {
-    createUserController
+    createUser
 }
