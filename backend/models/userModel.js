8d373d32b60439352266c80f54dbaae5614ec31a
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-13 15:35:45 +0200

    Created createUser to userModel.js
    Created function to handle creating a new user. Also added bcrypt library to hash password.

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const pgPool = require('../config/connection.js')
+const bcrypt = require('bcrypt')
+
+const sql = {
+    create: 'INSERT INTO users (uname, pw, email) VALUES ($1, $2, $3)'
+}
+
+const createUser = async (userData) => {
+    const { uname, pw, email } = userData;
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(pw, saltRounds);
+    const values = [uname, passwordHash, email];
+    try {
+        const result = await pgPool.query(sql.create, values);
+        return result;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }    
+}
+
+module.exports = {
+    createUser
+}
