ed1cc1b1f0cca58c8577701b2c5afeb728d0f45f
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-13 15:52:20 +0200

    Updated app.js to include userRoutes
    Added userRoutes to app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+// Routes
+const userRoutes = require('./routes/userRoutes.js');
+
+app.use('/users', userRoutes);
+
 // Server start
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
