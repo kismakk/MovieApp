af12917923d2d5a4a2f15b4a3d6eae3effde28fb
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-15 11:35:26 +0200

    Linted and modified userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,22 @@
-const user = require('../models/userModel.js')
+const user = require('../models/userModel.js');
 
 const createUser = async (req, res) => {
-    const userData = req.body;
-    if (!userData.uname || !userData.pw || !userData.email) {
-        return res.status(400).json({error: 'Missing required fields'});
+  const userData = req.body;
+  if (!userData.uname || !userData.pw || !userData.email) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+  try {
+    const result = await user.createUser(userData);
+    res.status(201).json({ message: 'User created succesfully', username: result.rows[0].uname });
+  } catch (error) {
+    if (error.message === 'Email already in use') {
+      return res.status(400).json({ error: error.message });
     }
-    try {
-        const result = await user.createUser(userData);
-        res.status(201).json({message: 'User created succesfully'});
-    } catch (error) {
-        if (error.message === 'Email already in use') {
-            return res.status(400).json({error: error.message});
-        }
-        console.log('Error in createUser', error);
-        res.status(500).json({error: 'Internal server error'});
-    }
-}
+    console.log('Error in createUser', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 module.exports = {
-    createUser
-}
+  createUser
+};
