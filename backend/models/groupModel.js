9afde732b192851cc408670271ac6138de243d4f
laralaamanen <lara.laamanen@gmail.com>
2023-11-17 09:58:56 +0200

    added getGroupInfo
    endpoint and CRUD

---
+++ backend/models/groupModel.js
@@ -0,0 +1,20 @@
+const pgPool = require('../config/connection.js');
+
+const sql = {
+  getGroupInfo: 'SELECT groups_name, groups_description FROM groups WHERE groups_name = $1' // group-name -> group name, group description
+};
+
+const getGroupInfo = async (groupName) => {
+  try {
+    const result = await pgPool.query(sql.getGroupInfo, [groupName]);
+    return result.rows[0];
+  } catch (error) {
+    console.error('Error getting group information:', error);
+    throw error;
+  }
+};
+
+module.exports = {
+  getGroupInfo,
+  sql
+};
--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -6,7 +6,7 @@ const jwt = require('../auth/auth.js');
 const groupController = require('../controllers/groupController.js');
 
 router.post('/create', upload.none(), jwt.auth, groupController.createGroup);
-router.get('/', upload.none(), groupController.getGroups);
+router.get('/:groupName', upload.none(), groupController.getGroupInfo);
 router.delete('/delete/:id', upload.none(), groupController.deleteGroup); // TODO: group auth, only for group owner
 router.put('/edit/:id', upload.none(), groupController.updateGroup); // TODO: group auth, only for group owner
 
