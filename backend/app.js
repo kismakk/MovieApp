bbd34a4d157724607396a763a2c42b5504931bfa
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-17 14:22:13 +0200

    Added cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,20 @@ require('dotenv').config();
 const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const { errorHandler, notFound } = require('./middleware/errorhandler.js');
 const app = express();
 
-// Require routes
-const userRoutes = require('./routes/userRoutes.js');
-const groupRoutes = require('./routes/groupRoutes.js');
-
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+// Require routes
+const userRoutes = require('./routes/userRoutes.js');
+const groupRoutes = require('./routes/groupRoutes.js');
 
 // Routes
 app.use('/users', userRoutes);
