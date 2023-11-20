a9e250e3bd17f5c92de31ac3b281e30e0eb347cb
Jaakko Kauppila <76628873+Kaubbila@users.noreply.github.com>
2023-11-21 00:00:27 +0200

    Favourite endpoints checkpoint commit

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,21 @@ const cookieParser = require('cookie-parser');
 const { errorHandler, notFound } = require('./middleware/errorhandler.js');
 const app = express();
 
-app.use(cookieParser());
+// Require routes
+const userRoutes = require('./routes/userRoutes.js');
+/* const groupRoutes = require('./routes/groupRoutes.js'); */
+const favouriteRoutes = require('./routes/favouriteRoutes.js');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// Require routes
-const userRoutes = require('./routes/userRoutes.js');
-const groupRoutes = require('./routes/groupRoutes.js');
-
 // Routes
 app.use('/users', userRoutes);
-// app.use('/groups', groupRoutes);
+/* app.use('/groups', groupRoutes); */
+app.use('/favourites', favouriteRoutes);  
 
 // Server start
 const port = process.env.PORT || 3001;
--- backend/models/favouriteModel.js
+++ backend/models/favouriteModel.js
@@ -1,23 +1,63 @@
 const pgPool = require('../config/connection.js');
 const bcrypt = require('bcrypt');
-
+//Täällä tehdään peruslogiikkahaku.
 const sql = {
-  addFavourite: 'INSERT INTO favourites ',
-  getAllFavourites: '',
-  deleteFavourite: 'SELECT * FROM users WHERE email = $1'
+  addFavourite: 'INSERT INTO favourites (id_user, id_group, movie_id, series_id, name, avatar VALUES ($1, $2, $3, $4, $5, $6)',
+  getAllFavouritesUser: 'SELECT * FROM favourites WHERE id_user = $1',
+  getAllFavouritesGroup: 'SELECT * FROM favourites WHERE id_group = $1',
+  deleteFromUser: 'DELETE from favourites where id_user = $1',
+  deleteFromGroup: 'DELETE from favourites where id_group = $1'
+};
+//Selvitä mitkä arvot saa idUser ja idGroup
+//Deleten arvo kans 
+const addToFavourites = async(addByUser) => {
+  const { idUser, idGroup, movieId, seriesId, name, avatar } = addByUser;
+  try {
+    await pgPool.query(sql.addFavourite,[idUser, idGroup, movieId, seriesId, name, avatar])
+  } catch (error) {
+    console.log('Failed to add favourites', error);
+    return error;
+  }
 };
 
-const addFavourite = async() => {
-
+const getAllFavourites = async(getByUser) => {
+  const { idUser, idGroup} = getByUser;
+  if(idGroup === '') {
+    try {
+      await pgPool.query(sql.getAllFavouritesUser,[idUser])
+    } catch (error) {
+      console.log('Failed to get favourites from user', error);
+      return error;
+    }
+  } else {
+    try {
+      await pgPool.query(sql.getAllFavouritesGroup,[idGroup])
+    } catch (error) {
+      console.log('Failed to get favourites from group', error);
+      return error;
+    }
+  }
 };
 
-const getAllFavourite = async() => {
-
-};
-
-const deleteFavourite = async() => {
-
+const deleteFavourite = async(deleteById) => {
+  const {idUser, idGroup} = deleteById;
+  if(idGroup === '') {
+    try {
+      await pgPool.query(sql.deleteById,[idUser])
+    } catch (error) {
+      console.log('Failed to delete from user', error);
+    }
+  } else { 
+    try {
+      await pgPool.query(sql.deleteById,[idGroup])
+    } catch (error) {
+      console.log('Failed to delete from group', error);
+    }
+  }
 };
 
 module.exports = {
+  addToFavourites,
+  getAllFavourites,
+  deleteFavourite
 };
