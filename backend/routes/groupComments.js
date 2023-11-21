5fcae225f1a4cb9ed13128f17dc902f7864b6c2b
Deforested <Teemuyritys@gmail.com>
2023-11-21 10:37:38 +0200

    updatet endpoints for groupcommenting

--- backend/routes/groupComments.js
+++ backend/routes/groupComments.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
-// const jwt = require('../auth/auth.js');
 const commentController = require('../controllers/commentController.js');
 
 router.get('/', upload.none(), commentController.getComments);
+// Alustettu routesit deletelle ja postille.
 // router.delete('/delete/:id', upload.none(), jwt.auth, commentController.deleteComment);
 // router.post('/comment/:id', upload.none(), jwt.auth, commentController.postComment);
 
