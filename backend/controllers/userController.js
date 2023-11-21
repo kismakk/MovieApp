3e8b6a45413ae641762d0aafedabf4264322c9d3
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-21 10:31:37 +0200

    Modified signup and signin
    Signup: Modified handling the result to be more readable and understandable
    
    Signin: Updated erorhandling to be consistent with the rest of the app

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,8 +15,8 @@ const createUser = async (req, res, next) => {
       res.status(400);
       throw new Error('Email already in use');
     }
-    const result = await user.createUser(userData);
-    const userId = result.rows[0].id_users;
+    const dbResult = await user.createUser(userData);
+    const { id_users: userId } = dbResult.rows[0].id_users;
     jwt.createToken(res, userId);
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
@@ -24,24 +24,25 @@ const createUser = async (req, res, next) => {
   }
 };
 
-const signIn = async (req, res) => {
+const signIn = async (req, res, next) => {
   const uname = req.body.uname;
   const pw = req.body.pw;
   try {
     const dbResult = await user.getPasswordAndId(uname);
     if (!dbResult) {
-      return res.status(404).json({ error: 'User not found' });
+      res.status(404);
+      throw new Error('User not found');
     }
     const { pw: pwHash, id_users: userId } = dbResult[0];
     const isAuthenticated = await bcrypt.compare(pw, pwHash);
     if (!isAuthenticated) {
-      return res.status(401).json({ error: 'Invalid username or password' });
+      res.status(401);
+      throw new Error('Invalid credentials');
     }
     jwt.createToken(res, userId);
     res.status(200).json({ message: 'User signed in successfully' });
   } catch (error) {
-    console.log('Error in signIn: ', error.message);
-    res.status(500).json({ error: 'Internal server error' });
+    next(error);
   }
 };
 
