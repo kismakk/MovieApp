aa02839f9922a86a42fa52f20ed493b241dd2c60
Plufin <118924716+Plufin@users.noreply.github.com>
2023-11-22 22:14:13 +0200

    Mainly organized components and applied styles in the frontend

---
+++ frontend/src/components/header/styles/header.js
@@ -0,0 +1,9 @@
+const headerStyles = {
+  header: {
+    backgroundColor: '#333',
+    color: '#000',
+    padding: '1rem',
+  },
+};
+
+export default headerStyles;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home testi</h1>
+      <Header />
+      {/* Other content for the Home page */}
+    </div>
+  );
+}
+
+export default Home;
