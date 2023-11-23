f2a5a632c0fb17fa97f74716f16649e9c574f186
Teemu Yritys <teemuyritys@gmail.com>
2023-11-23 09:29:38 +0200

    Alotettu deleten luonti

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -17,10 +17,20 @@ const postComments = async (req, res) => {
   const userComments = req.body.user_comments;
   try {
     const postComments = await comment.postComments(userComments, idUsers, idGroups);
-    res.status(200).json({ message: 'Success', postComments })
+    res.status(200).json({ message: 'Comment posted', postComments })
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
 };
 
-module.exports = { getComments, postComments};
+const deleteComment = async (req, res) => {
+  const commentId = req.params.id_comments;
+  try {
+    const deletedComment = await comment.deleteComment(commentId);
+    res.status(200).json({ message: 'Comment deleted', deletedComment })
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+module.exports = { getComments, postComments, deleteComment };
--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -3,7 +3,7 @@ const pgPool = require('../config/connection.js');
 const sql = {
   postComment: 'INSERT INTO group_comments (user_comments, id_users, id_groups) VALUES ($1, $2, $3) RETURNING *',
   getComments: 'SELECT user_comments FROM group_comments WHERE id_groups = $1',
-  deleteComment: 'DELETE FROM group_comments WHERE id_comments = $1'
+  deleteComment: 'DELETE FROM group_comments WHERE id_comments = $1 RETURNING *'
 };
 
 const getComments = async (idComments) => {
@@ -34,4 +34,18 @@ const postComments = async (userComments, idUsers, idGroups) => {
   }
 };
 
-module.exports = { getComments, postComments};
+const deleteComment = async (commentId) => {
+  try {
+    const result = await pgPool.query(sql.deleteComment, [commentId]);
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      throw new Error ('Comment not found');
+    }
+  } catch (error) {
+    console.log('Error in deleteComment', error);
+    throw error;
+  }
+};
+
+module.exports = { getComments, postComments, deleteComment};
