bdc5803327fc6f18f34f21414f48b0de5fe7a904
Plufin <118924716+Plufin@users.noreply.github.com>
2023-11-25 12:24:04 +0200

    Implemented responsive design for global use: Adjusted fonts and grid layouts

--- frontend/src/components/global/index.js
+++ frontend/src/components/global/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import GlobalStyle from '../styles/global';
+import GlobalStyle from './styles/global';
 
-// Tämä komponentti voi olla tyhjä, koska GlobalStyle ei tarvitse lapsielementtejä.
 function Global() {
   return <GlobalStyle />;
 }
