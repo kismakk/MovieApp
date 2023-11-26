719cd7d813be4bffc6f268b2e67503e782a7a986
Teemu Yritys <teemuyritys@gmail.com>
2023-11-26 20:55:40 +0200

    testit tehty getille ja postille, delete kesken

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -2,8 +2,14 @@ const comment = require('../models/commentModel');
 
 const getComments = async (req, res) => {
   const groupId = req.query.id_groups;
+  if (!groupId) {
+    return res.status(400).json({ error: 'Missing groupId' });
+  }
   try {
     const getComments = await comment.getComments(groupId);
+    if (!getComments || getComments.length === 0) {
+      return res.status(404).json({ error: 'No comments found for your group' });
+    }
     console.log(getComments);
     res.status(200).json({ message: 'Success', getComments });
   } catch (error) {
@@ -30,7 +36,6 @@ const postComments = async (req, res) => {
 const deleteComment = async (req, res, next) => {
   const commentId = req.params.id;
   const userId = res.locals.userId;
-  console.log(res.locals);
 
   try {
     const deletedRowCount = await comment.deleteComment(commentId, userId);
@@ -40,7 +45,7 @@ const deleteComment = async (req, res, next) => {
     } else if (deletedRowCount === undefined) {
       res.status(500).json({ error: 'Something went wrong, try again later' });
     } else {
-      return res.status(200).json({ message: 'Comment deleted successfully.' });
+      return res.status(200).json({ message: 'Comment deleted successfully' });
     }
   } catch (error) {
     res.status(404).json({ error: error.message });
