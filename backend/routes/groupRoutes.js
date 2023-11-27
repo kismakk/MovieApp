82e258a2270b6ba8b6e0797afdb1922a0b0c8fb5
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-27 15:45:14 +0200

    Enabled createGroup route in groupRoutes.js

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -5,7 +5,7 @@ const upload = multer({ dest: 'uploads/' });
 const jwt = require('../auth/auth.js');
 const groupController = require('../controllers/groupController.js');
 
-// router.post('/create', upload.none(), jwt.auth, groupController.createGroup);
+router.post('/create', upload.none(), jwt.auth, groupController.createGroup);
 router.get('/:groupName', upload.none(), groupController.getGroupInfo);
 // router.delete('/delete/:id', upload.none(), groupController.deleteGroup); // TODO: group auth, only for group owner
 // router.put('/edit/:id', upload.none(), groupController.updateGroup); // TODO: group auth, only for group owner
