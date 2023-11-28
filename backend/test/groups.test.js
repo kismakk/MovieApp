6f853a83e0d39fa8da55f8100c1d9458944adda0
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-28 11:49:39 +0200

    Refactor group creation tests

--- backend/test/groups.test.js
+++ backend/test/groups.test.js
@@ -8,71 +8,75 @@ const jwt = require('jsonwebtoken');
 describe('Group functionality', function () {
   const group = { groupName: 'test', groupDescription: 'This is a test description' };
   let token;
-  let userId; // Add a user to the database before running the tests, then add the userId here
-  before(function (done) {
+  const userId = 2; // Add a user to the database before running the tests, then add the userId here
+
+  before(function () {
     token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-    done();
   });
+
   describe('POST /groups/create', function () {
-    it('should return a status code of 201 and should return a message of "Group created successfully"', function (done) {
-      request(app)
+    it('should return a status code of 201 and should return a message of "Group created successfully"', async function () {
+      const res = await request(app)
         .post('/groups/create')
         .send(group)
-        .set('Cookie', [`uJwt=${token}`])
-        .end((_err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body.message).to.equal('Group created successfully');
-          done();
-        });
+        .set('Cookie', [`uJwt=${token}`]);
+
+      expect(res.status).to.equal(201);
+      expect(res.body.message).to.equal('Group created successfully');
     });
-    it('shouldn\'t create a group if the group name is already in use', function (done) {
-      request(app)
+
+    it('should return a status code 403 if the user is not logged in', async function () {
+      const res = await request(app)
+        .post('/groups/create')
+        .send(group);
+
+      expect(res.status).to.equal(403);
+      expect(res.body.error).to.equal('Not authorized');
+    });
+
+    it('shouldn\'t create a group if the group name is already in use', async function () {
+      const res = await request(app)
         .post('/groups/create')
         .send(group)
-        .set('Cookie', [`uJwt=${token}`])
-        .end((_err, res) => {
-          expect(res.status).to.equal(400);
-          expect(res.body.error).to.equal('Group already exists');
-          done();
-        });
+        .set('Cookie', [`uJwt=${token}`]);
+
+      expect(res.status).to.equal(400);
+      expect(res.body.error).to.equal('Group already exists');
     });
-    it('should return an error if the group name is missing', function (done) {
+
+    it('should return an error if the group name is missing', async function () {
       const group = { groupDescription: 'This is a test description' };
-      request(app)
+      const res = await request(app)
         .post('/groups/create')
         .send(group)
-        .set('Cookie', [`uJwt=${token}`])
-        .end((_err, res) => {
-          expect(res.status).to.equal(400);
-          expect(res.body.error).to.equal('Group name is required');
-          done();
-        });
+        .set('Cookie', [`uJwt=${token}`]);
+
+      expect(res.status).to.equal(400);
+      expect(res.body.error).to.equal('Group name is required');
     });
-    it('should return an error if the group name is an empty string', function (done) {
+
+    it('should return an error if the group name is an empty string', async function () {
       const group = { groupName: '', groupDescription: 'This is a test description' };
-      request(app)
+      const res = await request(app)
         .post('/groups/create')
         .send(group)
-        .set('Cookie', [`uJwt=${token}`])
-        .end((_err, res) => {
-          expect(res.status).to.equal(400);
-          expect(res.body.error).to.equal('Group name is required');
-          done();
-        });
+        .set('Cookie', [`uJwt=${token}`]);
+
+      expect(res.status).to.equal(400);
+      expect(res.body.error).to.equal('Group name is required');
     });
-    it('should return userId and groupId in the response', function (done) {
+
+    it('should return userId and groupId in the response', async function () {
       const group = { groupName: 'test2', groupDescription: 'This is a test description' };
-      request(app)
+      const res = await request(app)
         .post('/groups/create')
         .send(group)
-        .set('Cookie', [`uJwt=${token}`])
-        .end((_err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body.message).to.equal('Group created successfully');
-          expect(res.body.group.id_groups).to.be.a('number');
-          expect(res.body.userId).to.be.a('number');
-          done();
-        });
+        .set('Cookie', [`uJwt=${token}`]);
+
+      expect(res.status).to.equal(201);
+      expect(res.body.message).to.equal('Group created successfully');
+      expect(res.body.group.id_groups).to.be.a('number');
+      expect(res.body.userId).to.be.a('number');
     });
   });
 });
