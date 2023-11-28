c8ca5f80f6b30108be178a5544eb078bf60d47f3
Plufin <118924716+Plufin@users.noreply.github.com>
2023-11-28 14:38:30 +0200

    Created tests for reviews

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,6 @@ const cookieParser = require('cookie-parser');
 const { errorHandler, notFound } = require('./middleware/errorhandler.js');
 const app = express();
 
-// Require routes
-const userRoutes = require('./routes/userRoutes.js');
-const commentRoutes = require('./routes/groupComments.js');
-const groupRoutes = require('./routes/groupRoutes.js');
-
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -18,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-
 // Require routes
 const userRoutes = require('./routes/userRoutes.js');
+const commentRoutes = require('./routes/groupComments.js');
 const groupRoutes = require('./routes/groupRoutes.js');
 const reviewRoutes = require('./routes/reviewRoutes.js');
 
