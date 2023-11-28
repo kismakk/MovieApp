54063ddca380995a88ea9a0eb8a64d0d720fed01
Teemu Yritys <teemuyritys@gmail.com>
2023-11-28 12:26:58 +0200

    tehty pohja globaleilla componenteillä

--- frontend/src/pages/Discover.js
+++ frontend/src/pages/Discover.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Global from '../components/global/styles/global';
+import Header from '../components/global/Header';
+import NavBar from '../components/global/NavBar';
 function Discover() {
   return (
-    <div>Discover</div>
+    <div className='container'>
+      <Global />
+      <div className='header'>
+        <header>
+          <Header />
+        </header>
+        <div className='content'>
+          <nav>
+            <NavBar />
+          </nav>
+          <main>
+            <h2>Heissulivei</h2>
+          </main>
+        </div>
+      </div>
+    </div>
   )
 }
 
