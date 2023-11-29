cf337bd2e3709a8cf8f45610cb03c5651c94834b
laralaamanen <lara.laamanen@gmail.com>
2023-11-29 15:11:59 +0200

    deleteGroup routing fixed

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -7,7 +7,7 @@ const groupController = require('../controllers/groupController.js');
 
 router.post('/create', upload.none(), jwt.auth, groupController.createGroup);
 router.get('/:groupName', upload.none(), groupController.getGroupInfo);
-router.delete('/delete/:id', upload.none(), jwt.auth(), groupController.deleteGroup);
+router.delete('/delete/:id', upload.none(), jwt.auth, groupController.deleteGroup);
 // router.put('/edit/:id', upload.none(), groupController.updateGroup); // TODO: group auth, only for group owner
 router.get('/', upload.none(), groupController.getAllGroups);
 module.exports = router;
