b5cb86a74417295592973ab9055ba3bfe22771d2
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-30 16:18:55 +0200

    Add button components for settings nav

---
+++ frontend/src/components/settingsComponents/GeneralButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledButton } from './StyledButton'
+
+const GeneralButton = () => {
+  return (
+    <>
+      <button style={StyledButton}>General</button>
+    </>
+  )
+}
+
+export default GeneralButton
---
+++ frontend/src/components/settingsComponents/UserButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledButton } from './StyledButton'
+
+const UserButton = () => {
+  return (
+    <>
+      <button style={StyledButton}>User</button>
+    </>
+  )
+}
+
+export default UserButton
