6bdef64a6f98d8f4f3e24f4aec04551f55b8cb06
kismakk <119887923+kismakk@users.noreply.github.com>
2023-11-30 20:06:27 +0200

    Revert "Remove import statement for App.css"
    This reverts commit 7511ab7dd6ceaff1484de1ce868f4a9cbd48d350.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import 'tailwindcss/tailwind.css';
 import { BrowserRouter, Routes, Route, Link, useNavigate, useParams, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
