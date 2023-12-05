ae9551f0f33aa4d1c469a627fcf760f587c32807
Plufin <118924716+Plufin@users.noreply.github.com>
2023-12-05 14:52:59 +0200

    Limit titles to two rows, truncating with '...' if exceeded.

--- frontend/src/components/global/MediaList.js
+++ frontend/src/components/global/MediaList.js
@@ -1,36 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const MediaList = ({ media, title }) => {
+const MediaListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const MediaItem = styled.div`
+  width: 18%;
+  margin-bottom: 5px;
+  box-sizing: border-box;
+  position: relative;
+`;
+
+const MediaTitle = styled.p`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.5em;
+  margin: 0;
+  text-align: center;
+  max-width: 100%;
+`;
+
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 12px;
+`;
+
+const MediaList = ({ media }) => {
   const displayedMedia = media.slice(0, 5);
 
   return (
-    <div style={mediaListStyle}>
+    <MediaListContainer>
       {displayedMedia.map((item) => (
-        <div key={item.id} style={mediaItemStyle}>
-          <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title || item.name} style={imgStyle} />
-          <p className='mediaTitle'>{item.title || item.name}</p>
-        </div>
+        <MediaItem key={item.id}>
+          <Image src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title || item.name} />
+          <MediaTitle>{item.title || item.name}</MediaTitle>
+        </MediaItem>
       ))}
-    </div>
+    </MediaListContainer>
   );
-};
-
-const mediaListStyle = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'space-between',
-};
-
-const mediaItemStyle = {
-  width: '18%',
-  marginBottom: '5px',
-  boxSizing: 'border-box',
-};
-
-const imgStyle = {
-  width: '100%',
-  height: 'auto',
-  borderRadius: '12px',
 };
 
 export default MediaList;
