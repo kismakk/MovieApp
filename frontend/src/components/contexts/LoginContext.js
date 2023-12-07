e631403fe3f8f0d137d42644b949f3c68f8c550e
kismakk <119887923+kismakk@users.noreply.github.com>
2023-12-07 13:20:53 +0200

    Add LoginContext and LoginProvider components

---
+++ frontend/src/components/contexts/LoginContext.js
@@ -0,0 +1,52 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+
+const LoginContext = createContext();
+
+export const useLogin = () => {
+  return useContext(LoginContext);
+};
+
+export const LoginProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    return storedLoginStatus ? JSON.parse(storedLoginStatus) : false;
+  });
+
+  useEffect(() => {
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    if (storedLoginStatus) {
+      setIsLoggedIn(JSON.parse(storedLoginStatus));
+    }
+  }, []);
+
+  const login = () => {
+    axios.post('http://localhost:3001/users/signin', { uname: 'kismakk', pw: 'test' }, { withCredentials: true })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error('Axios error: ', err);
+      })
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', JSON.stringify(true));
+  };
+
+  const logout = () => {
+    axios.post('http://localhost:3001/users/signout', {}, { withCredentials: true })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error('Axios error: ', err);
+      })
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return (
+    <LoginContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </LoginContext.Provider>
+  );
+};
