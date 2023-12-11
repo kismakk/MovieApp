ca8584a08b230e0e604f162e6e0d910bba7cdb8a
Teemu Yritys <teemuyritys@gmail.com>
2023-12-11 20:37:01 +0200

    korjattu bugi joka rikko filteröinnin

--- frontend/src/components/discoverComponents/content.js
+++ frontend/src/components/discoverComponents/content.js
@@ -10,110 +10,108 @@ const ImageGrid = () => {
   const [selectedMediaType, setSelectedMediaType] = useState("All");
   const [selectedSortBy, setSelectedSortBy] = useState("Sort By");
   const [selectedGenre, setSelectedGenre] = useState("Genres");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalDisplayCount, setTotalDisplayCount] = useState(15);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const apiKey = process.env.REACT_APP_TMDB_API_KEY;
-        const params = { api_key: apiKey, page: currentPage };
+        const params = { api_key: apiKey };
 
         if (selectedSortBy === "Sort By") {
           const movies = await GetContent("discover/movie", params);
           const tvShows = await GetContent("discover/tv", params);
 
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);
+          setAllMovies(movies);
+          setAllShows(tvShows);
         } else if (selectedSortBy === "Popular"){
-          const movies = await GetContent("movie/popular?language=en-US", {
+          const movies = await GetContent("movie/popular?language=en-US&page=1", {
             api_key: apiKey,
           });
-          const tvShows = await GetContent("tv/popular?language=en-US", {
+          const tvShows = await GetContent("tv/popular?language=en-US&page=1", {
             api_key: apiKey,
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);
+          setAllMovies(movies);
+          setAllShows(tvShows);
         } else if (selectedSortBy === "Now Playing") {
-          const movies = await GetContent("movie/now_playing?language=en-US", {
+          const movies = await GetContent("movie/now_playing?language=en-US&page=1", {
             api_key: apiKey,            
           });
-          const tvShows = await GetContent("tv/on_the_air?language=en-US", {
+          const tvShows = await GetContent("tv/on_the_air?language=en-US&page=1", {
             api_key: apiKey,            
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);
+          setAllMovies(movies);
+          setAllShows(tvShows);
         }
         else if (selectedSortBy === "Top Rated") {
-            const movies = await GetContent("movie/top_rated?language=en-US", {
+            const movies = await GetContent("movie/top_rated?language=en-US&page=1", {
             api_key: apiKey,            
           });
           
-          const tvShows = await GetContent("tv/top_rated?language=en-US", {
+          const tvShows = await GetContent("tv/top_rated?language=en-US&page=1", {
             api_key: apiKey,            
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);
+          setAllMovies(movies);
+          setAllShows(tvShows);
         } if (selectedGenre === "Animation") {
           const genreId = genreNameToId("Animation");
-          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US", {
+          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US", {
+          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);
+          setAllMovies(movies);
+          setAllShows(tvShows);
         }  else if (selectedGenre === "Comedy") {
           const genreId = genreNameToId("Comedy");
-          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US", {
+          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US", {
+          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);        
+          setAllMovies(movies);
+          setAllShows(tvShows);       
         } else if (selectedGenre === "Crime") {
           const genreId = genreNameToId("Crime");
-          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US", {
+          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US", {
+          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);         
+          setAllMovies(movies);
+          setAllShows(tvShows);        
         }  else if (selectedGenre === "Drama") {
           const genreId = genreNameToId("Drama");
-          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US", {
+          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US", {
+          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);        
+          setAllMovies(movies);
+          setAllShows(tvShows);      
         } else if (selectedGenre === "Family") {
           const genreId = genreNameToId("Family");
-          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US", {
+          const movies = await GetContent("discover/movie?include_adult=true&include_video=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US", {
+          const tvShows = await GetContent("discover/tv?include_adult=true&include_null_first_air_dates=false&language=en-US&page=1", {
             api_key: apiKey,
             with_genres: genreId,     
           });
-          setAllMovies((prevMovies) => [...prevMovies, ...movies]);
-          setAllShows((prevShows) => [...prevShows, ...tvShows]);         
+          setAllMovies(movies);
+          setAllShows(tvShows);       
         } 
       } catch (error) {
         console.error("Error fetching content", error);
@@ -121,12 +119,8 @@ const ImageGrid = () => {
     };
 
     fetchData();
-  }, [selectedMediaType, selectedSortBy, selectedGenre, currentPage]);
+  }, [selectedMediaType, selectedSortBy, selectedGenre]);
 
-  const handleLoadMore = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-    setTotalDisplayCount((prevDisplayCount) => prevDisplayCount + 15);
-  };
 
   return (
     <div>
@@ -134,17 +128,16 @@ const ImageGrid = () => {
       <Grid>
         {selectedMediaType === "All" && (
           <>
-            <MediaList media={allMovies} mediaType="movies" displayCount={totalDisplayCount} />
-            <MediaList media={allShows} mediaType="series" displayCount={totalDisplayCount} />
+            <MediaList media={allMovies} mediaType="movies" displayCount={15} />
+            <MediaList media={allShows} mediaType="series" displayCount={15} />
           </>
         )}
         {selectedMediaType === "Movies" && (
-          <MediaList media={allMovies} mediaType="movies" displayCount={totalDisplayCount} />
+          <MediaList media={allMovies} mediaType="movies" displayCount={20} />
         )}
         {selectedMediaType === "Shows" && (
-          <MediaList media={allShows} mediaType="series" displayCount={totalDisplayCount} />
+          <MediaList media={allShows} mediaType="series" displayCount={20} />
         )}
-        <LoadMoreButton onClick={handleLoadMore}>Load More</LoadMoreButton>
       </Grid>
     </div>
   );
@@ -159,20 +152,5 @@ const Grid = styled.div`
     top: 20%;
   }
 `;
-const LoadMoreButton = styled.button`
-  margin-top: 20px;
-  background-color: #1F2626;
-  border: none;
-  border-radius: 8px;
-  color: #F3F3E7;
-  font-family: Montserrat;
-  font-size: 18px;
-  padding: 10px;
-  cursor: pointer;
-  width: 100%;
-  max-width: 200px;
-  align-self: center;
-  margin-bottom: 20px;
-  margin-left: 40%;
-`;
+
 export default ImageGrid;
