8e2811cddff44cf1d472ae434beaade6f9074671
Jaakko Kauppila <76628873+Kaubbila@users.noreply.github.com>
2023-12-12 11:34:51 +0200

    Profile preview initialized

---
+++ frontend/src/pages/ProfilePreview.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/global/Header';
+import Global from '../components/global';
+import NavBar from '../components/global/NavBar';
+import Avatar from '../components/profileComponents/Avatar'
+import Groups from '../components/profileComponents/Groups'
+import Favourites from '../components/profileComponents/Favourites'
+import Comments from '../components/profileComponents/Comments'
+import axios from 'axios';
+
+function Profile() {
+
+  const [avatarName, setAvatarName] = useState('');
+  const [groups, setGroups] = useState('');
+  const [favourites, setFavourites] = useState('');
+useEffect(() => {
+      //Haetaan userNimellä id ja haetaan sitten samalla kaikki muutkin id:tä vastaan:-)
+      
+}, []);
+
+  return (
+    <div className="container">
+      <Global />
+      <header>
+        <Header />
+      </header>
+      <div className="content">
+        <nav>
+          <NavBar />
+        </nav>
+        <main>
+          <div className="avatarName">
+            <Avatar userData={avatarName}/>
+          </div>
+          <div className="groups">
+            <Groups groupsData={groups}/>
+          </div>
+          <div className="favourites">
+            <Favourites favouritesData={favourites}/>
+          </div>
+        </main>
+        <div className="side-section">
+          <Comments/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile
