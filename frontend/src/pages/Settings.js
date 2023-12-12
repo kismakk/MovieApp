aca2a01b59993ee2a700a3459a56fcbdc938b4c5
kismakk <119887923+kismakk@users.noreply.github.com>
2023-12-12 13:39:05 +0200

    Add styled-components to Settings.js to solve navbar blocking content

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import Header from '../components/global/Header'
 import Global from '../components/global/styles/global'
 import Settingsnav from '../components/settingsComponents/Settingsnav'
@@ -30,19 +31,28 @@ function Settings() {
               <Settingsnav />
             </div>
           </div>
-          <main>
+          <MainContent>
             <div style={InfoContainer}>
               <GeneralInfo />
               <UserInfo />
               <GroupInfo />
               {isLoggedIn && <SignOut />}
             </div>
-          </main>
+          </MainContent>
         </div>
       </div>
     </>
   )
 }
+
+const MainContent = styled.main`
+  margin: 0 1rem;
+  flex: 2;
+
+  @media (max-width: 900px) {
+    padding-bottom: 6rem;
+  }
+`;
 
 const InfoContainer = {
   display: 'flex',
