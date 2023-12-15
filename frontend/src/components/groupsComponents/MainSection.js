d5e0449b8240789cafd7701198295fac17326b80
Plufin <118924716+Plufin@users.noreply.github.com>
2023-12-15 17:54:35 +0200

    Front ready for launch!

--- frontend/src/components/groupsComponents/MainSection.js
+++ frontend/src/components/groupsComponents/MainSection.js
@@ -1,54 +1,69 @@
-// groupsComponents/MainSection.js
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
 const MainSectionContainer = styled.div`
-  /* Add your styling for the main section container */
   flex: 1;
-  padding: 10px; /* Adjust as needed */
+  padding: 10px;
   display: flex;
   align-items: center;
-  flex-direction: column;
-  margin-right: 100px;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 0px;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    margin-right: 0px;
+  }
 `;
 
 const GroupDetailsContainer = styled.div`
-  /* Updated styling for the group details container */
   display: flex;
-  align-items: flex-start; /* Align items to the start of the container */
-  margin-bottom: 10px;
+  align-items: flex-start;
+  flex-direction: column;
 `;
 
 const Description = styled.p`
-  /* Add your styling for the description */
-  margin-top: 5px; /* Adjust margin as needed */
+  margin-bottom: 15px;
+
+  @media (max-width: 1000px) {
+    align-self: center;
+  }
 `;
 
 const MembersContainer = styled.div`
-  /* Add your styling for the members container */
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  margin-right: 90px;
+margin-left: -20px;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+overflow-y: auto;
+max-height: 100px;
+width: 100%;
 `;
 
 const Member = styled.div`
-  margin-right: -20px;
 `;
 
 const AvatarContainer = styled.div`
   text-align: left;
   display: flex;
   align-items: center;
-  margin: 1rem ; /* Adjust margins as needed */
+  margin: 1rem ;
+  border-radius: 10px;
+  padding: 5px;
 `;
 
 const GroupPicture = styled.img`
-  width: 188px; /* Set the desired width for the group picture */
-  height: 188px; /* Set the desired height for the group picture */
-  border-radius: 50%;
-  margin-right: 400px; /* Adjust margin to move the picture to the left */
+width: 188px;
+height: 188px;
+border-radius: 50%;
+margin-right: 50px;
+background-color: transparent;
+
+@media (max-width: 1000px) {
+  align-self: center;
+  margin-right: 0px;
+}
 `;
 
 const Picture = styled.img`
@@ -58,24 +73,30 @@ const Picture = styled.img`
 `;
 
 const GroupNameTitle = styled.h1`
-  font-size: 1.5rem;
-  text-align: center; /* Reset margin to ensure consistent spacing */
+margin-bottom: 0px;
+
+@media (max-width: 1000px) {
+  align-self: center;
 `;
 
 const Section = styled.h2`
-  margin-top: -7rem;
-  margin-right: 80px;
+margin-bottom: 5px;
+margin-top: 40px;
+
+@media (max-width: 1000px) {
+  font-size: 22px;
+}
 `;
 
-const MainSection = ({groupId}) => {
-  
+const MainSection = ({ groupId }) => {
+
   const [groupName, setGroupName] = useState('name');
   const [avatar, setAvatar] = useState('avatar');
   const [description, setDescription] = useState('description');
   const [members, setMembers] = useState([]);
-  
+
   useEffect(() => {
-    axios.get(`http://localhost:3001/groups/${groupId}`, {withCredentials: true})
+    axios.get(`http://localhost:3001/groups/${groupId}`, { withCredentials: true })
       .then((res) => {
         console.log(res.data)
         const group = res.data.groupInfo
@@ -86,7 +107,7 @@ const MainSection = ({groupId}) => {
       .catch((error) => {
         console.error(error);
       })
-    axios.get(`http://localhost:3001/groups/members/${groupId}`, {withCredentials: true})
+    axios.get(`http://localhost:3001/groups/members/${groupId}`, { withCredentials: true })
       .then((res) => {
         console.log(res.data)
         setMembers(res.data.groupMembers);
@@ -99,31 +120,26 @@ const MainSection = ({groupId}) => {
 
   return (
     <MainSectionContainer>
+      <GroupPicture src={avatar} />
       <GroupDetailsContainer>
-        {/* Group Picture */}
-        <GroupPicture src={avatar} />
-        {/* Group Name Title */}
-        <div>
-          <GroupNameTitle>{groupName}</GroupNameTitle>
-          <Description>{description}</Description>
-        </div>
+        <GroupNameTitle>{groupName}</GroupNameTitle>
+        <Description>{description}</Description>
+        <Section>Members</Section>
+        <MembersContainer>
+          {members.map((member) => (
+            <Member key={member.id_users}>
+              {/* Display member information */}
+              <AvatarContainer>
+                <Picture src={member.user_avatar} />
+                <div>
+                  <p>{member.uname}</p>
+                  {/* Add more member information as needed */}
+                </div>
+              </AvatarContainer>
+            </Member>
+          ))}
+        </MembersContainer>
       </GroupDetailsContainer>
-      {/* Members and Add Member Button */}
-      <Section>Members</Section>
-      <MembersContainer>
-        {members.map((member) => (
-          <Member key={member.id_users}>
-            {/* Display member information */}
-            <AvatarContainer>
-              <Picture src={member.user_avatar} />
-              <div>
-                <p>{member.uname}</p>
-                {/* Add more member information as needed */}
-              </div>
-            </AvatarContainer>
-          </Member>
-        ))}
-      </MembersContainer>
     </MainSectionContainer>
   );
 };
